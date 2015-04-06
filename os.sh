a1785f13319cc78ab6f6890005b24088d05a28a4
rcastellow <robert.castellow@gmail.com>
2015-04-06 19:25:42 -0400

    add startup script

---
+++ os.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export PYTHONPATH=$PYTHONPATH:./com/pac/cryptofx/algorithm:/com/pac/cryptofx/exchange/campx:/com/pac/cryptofx/exchange/cryptsy
+
+python ./com/pac/oshaughnessey/startOS.py
